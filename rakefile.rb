589d109171e63c4ff723cbe2ef3fe4a0cac6189d
Denis Potapov <denis.potapov@esr-nb-1410.esrlabs.local>
2020-02-24 17:31:10 +0100

    tmp skip signing fix

--- rakefile.rb
+++ rakefile.rb
@@ -990,6 +990,7 @@ task :prepare_to_deploy do
     else
       release_name += ".zip"
       cd "#{target_platform_alias}-unpacked" do
+        sh "ls"
         sh "zip -r ../#{release_name} ./*"
       end
     end
