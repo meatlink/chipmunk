ccd5110ca8c2243aa331d5a60ed4572b79350347
Denis Potapov <denis.potapov@esr-nb-1410.esrlabs.local>
2020-02-25 12:14:43 +0100

    tmp zip

--- rakefile.rb
+++ rakefile.rb
@@ -991,7 +991,9 @@ task :prepare_to_deploy do
       release_name += ".zip"
       cd "#{target_platform_alias}-unpacked" do
         sh "ls"
-        sh "zip -r \"..\\test.zip\" *"
+        # sh "Compress-Archive * ..\#{release_name}"
+        # sh "Compress-Archive * ..\#{release_name}"
+        # sh "zip -r \"..\\test.zip\" *"
         # sh "zip -r \"../#{release_name}\" ./*"
       end
     end
